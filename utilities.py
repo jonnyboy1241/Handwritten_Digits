e7327b35ee2492b469fe78ca5b1b4cfc1a81b0d2
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2021-01-12 12:48:52 -0600

    Upload trials of knn

--- utilities.py
+++ utilities.py
@@ -13,14 +13,6 @@ def view_image(image):
     plt.show()
 
 
-# This function displays an MNIST handwritten digit as a reduced 14x14 pixel image
-def view_reduced_image(image):
-    image = image.reshape(14, 14)
-    plt.gray()
-    plt.imshow(image)
-    plt.show()
-
-
 # Calculate the accuracy of the classification
 def compute_accuracy(classifications, labels):
     assert classifications.size == labels.size
@@ -40,3 +32,10 @@ def compute_time(begin_time):
     time_passed = time.time() - begin_time
     print('Time = %6.4f s' % (time_passed))
     return time_passed
+
+
+# This returns the set of images as a vector of values between 0-1
+# instead of just integers from 0-255
+def normalize_images(images):
+    return images.astype('float32') / 255.0
+    
